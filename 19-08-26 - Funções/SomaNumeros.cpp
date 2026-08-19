a6fa1ab0b96267ee8ade63c106bcffc42db5b834
Luiz <luizfelipe10707@gmail.com>
2026-08-19 11:32:50 -0300

    adding power calculation function

--- 19-08-26 - Funções/SomaNumeros.cpp
+++ 19-08-26 - Funções/SomaNumeros.cpp
@@ -13,14 +13,14 @@ int somar(int, int);
 int multiplicar(int, int);
 float dividir(int, int);
 int subtrair(int, int);
-
+int calcularPotencia(int, int);
 // inicio do main
 main() 
 {
 	setlocale(LC_ALL,"Portuguese");
-	int a, b, soma,mult,sub;
+	int a, b, soma,mult,sub,pot;
 	float div;
-	sub = mult = a = b = soma = 0;
+	pot = sub = mult = a = b = soma = 0;
 	div = 0.0;
 	
 	printf("digite o valor de A: ");
@@ -32,11 +32,13 @@ main()
 	mult = multiplicar(a,b);
 	div = dividir(a,b);
 	sub = subtrair(a,b);
-	
+	pot = calcularPotencia(a,b);
 	printf("a soma de %d e %d é: %d",a,b,soma);
 	printf("\na multiplicação de %d e %d é: %d",a,b,mult);
 	printf("\na divisão de %d por %d é: %.2f",a,b,div);
 	printf("\na subtração de %d por %d é: %d",a,b,sub);
+	printf("\n%d elevado a %d é: %d",a,b,pot);
+
 
 } //fim do main
 
@@ -63,6 +65,15 @@ float dividir(int a, int b) {
 int subtrair(int a, int b) {
 	int result = 0;
 	result = a-b;
+	return result;
+}
+//Função calcularPotencia
+int calcularPotencia(int a, int b) {
+	int result = a;
+	int i = 1;
+	for (i;i < b;i++ ) {
+		result = result*a;
+	}
 	return result;
 }
 //fim do programa
